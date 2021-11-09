c1b5d0caad7897b6ba2b0649bc73e677250f7f0f
Adrien Husson <adhusson@gmail.com>
2021-11-09 20:25:29 +0100

    rename mumbai to maticmum, it's the name used by metamask

--- packages/mangrove.js/src/constants.ts
+++ packages/mangrove.js/src/constants.ts
@@ -5,7 +5,7 @@ export const addresses = {
   kovan: {},
   ropsten: {},
   hardhat: require("./hardhatAddresses.json"),
-  mumbai: {
+  maticmum: {
     Mangrove: "0xF15A2ada64A9C3C14ca11359c8F1d22B063DB2AD",
     MgvReader: "0x5B2F8058Df0A0b7744FDb4fD0885FbCD2394194C",
     MgvOracle: "0xc74E3658C46B669E54449b0b371Fab6E070d3380",
