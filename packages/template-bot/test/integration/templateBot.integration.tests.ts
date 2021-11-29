62b26a0a6d12ab7baac57d5285c82b04c67b5c0a
Troels Damgaard <troels@tedconsulting.dk>
2021-11-29 16:31:46 +0100

    Add stub integration test

--- packages/template-bot/test/integration/templateBot.integration.tests.ts
+++ packages/template-bot/test/integration/templateBot.integration.tests.ts
@@ -1,17 +1,59 @@
 /**
  * Test that integration test infrastructure is working.
  */
-import { describe, it } from "mocha";
+
+import { afterEach, before, beforeEach, describe, it } from "mocha";
 import * as chai from "chai";
 const { expect } = chai;
 import chaiAsPromised from "chai-as-promised";
 chai.use(chaiAsPromised);
 import Mangrove from "@giry/mangrove-js";
+import { TemplateBot } from "../../src/TemplateBot";
+import * as hre from "hardhat";
+import "hardhat-deploy-ethers/dist/src/type-extensions";
+import { config } from "../../src/util/config";
+import { SignerWithAddress } from "hardhat-deploy-ethers/dist/src/signers";
 
+// TODO: Basic can-connect test - delete/update as needed
 describe("Can connect to Mangrove on local chain", () => {
   it("should be able to connect to Mangrove", function () {
     return expect(
       Mangrove.connect({ provider: this.test?.parent?.parent?.ctx.provider })
     ).to.eventually.be.fulfilled;
+  });
+});
+
+// TODO: integration tests - add/update as needed
+describe("GasUpdater integration tests", () => {
+  let botSigner: SignerWithAddress;
+  let mgv: Mangrove;
+
+  before(async function () {
+    //TODO:
+    // update "gasUpdater" below to be the named address (see packages/hardhat-utils/config/hardhat-mangrove-config.js)
+    // specific to this bot
+    botSigner = await hre.ethers.getNamedSigner("gasUpdater");
+  });
+
+  beforeEach(async function () {
+    mgv = await Mangrove.connect({
+      provider: this.test?.parent?.parent?.ctx.provider,
+      signer: botSigner,
+    });
+
+    const deployer = (await hre.ethers.getNamedSigners()).deployer;
+  });
+
+  afterEach(() => {
+    mgv.disconnect();
+  });
+
+  it("should be able to create a TemplateBot", async function () {
+    // setup
+    const templateBot = new TemplateBot(mgv);
+
+    // TODO: Test
+
+    // TODO: Assert
   });
 });
