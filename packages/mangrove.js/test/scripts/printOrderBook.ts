66b4321b2776de278cf3b177d9c3dfd067a7ffe9
V <v@MacBook-Pro-4.local>
2021-12-04 12:27:25 +0100

    added max number of offers to display

--- packages/mangrove.js/test/scripts/printOrderBook.ts
+++ packages/mangrove.js/test/scripts/printOrderBook.ts
@@ -1,5 +1,6 @@
 import { Mangrove } from "../../src";
 import type { Offer } from "../../src/types";
+import type { BookOptions } from "../../src/types";
 import chalk from "chalk";
 import yargs from "yargs";
 
@@ -15,14 +16,23 @@ const argv = yargs(process.argv.slice(2))
     describe: "the name of the quote token (WETH, DAI, USDC, ...)",
     type: "string",
   })
+  .positional("quote", {
+    demandOption: true,
+    describe: "the max number of offers to display",
+    type: "string",
+  })
   .help().argv;
 
 const main = async () => {
-  const mangrove = await Mangrove.connect(process.env["ETHEREUM_NODE_URL"]);
-  const [baseTokenName, quoteTokenName] = argv["_"];
+  const mangrove = await Mangrove.connect(process.env["MUMBAI_NODE_URL"]); // changed ETHEREUM_ to MUMBAI_
+  const [baseTokenName, quoteTokenName, maxOffersDisplayed] = argv["_"];
+  const numberOffersDisplayed = {
+    maxOffers: maxOffersDisplayed,
+  }; // added an arg to define the number of offers to display
   const market = await mangrove.market({
     base: baseTokenName,
     quote: quoteTokenName,
+    bookOptions: numberOffersDisplayed,
   });
   const { asks, bids } = market.book();
 
