0151473786e8330de76e279196c723a41e3afa2c
Adrien Husson <adhusson@gmail.com>
2021-12-13 14:08:01 +0100

    Fix Offer location in marketcleaner

--- packages/cleaning-bot/src/MarketCleaner.ts
+++ packages/cleaning-bot/src/MarketCleaner.ts
@@ -1,6 +1,7 @@
 import { logger } from "./util/logger";
 import { Market } from "@giry/mangrove.js/dist/nodejs/market";
-import { Offer } from "@giry/mangrove.js/dist/nodejs/types";
+import * as Types from "@giry/mangrove.js/dist/nodejs/types";
+type Offer = Types.Market.Offer;
 import { MgvToken } from "@giry/mangrove.js/dist/nodejs/mgvtoken";
 import { Provider } from "@ethersproject/providers";
 import { BigNumber, BigNumberish } from "ethers";
