13d7d7a2a83290a16cc01903f4445576cdd62e1b
JeanKa <jean@mangrove.exchange>
2022-01-17 17:51:30 +0100

    updating integration tests

--- packages/mangrove.js/test/integration/simplemaker.integration.test.ts
+++ packages/mangrove.js/test/integration/simplemaker.integration.test.ts
@@ -4,7 +4,7 @@ import { afterEach, beforeEach, describe, it } from "mocha";
 import { BigNumber } from "ethers";
 
 import assert from "assert";
-import { Mangrove, OfferLogic, LiquidityProvider as LP } from "../../src";
+import { Mangrove, OfferLogic, LiquidityProvider } from "../../src";
 
 import { Big } from "big.js";
 import { toWei } from "../util/helpers";
@@ -32,7 +32,7 @@ describe("SimpleMaker", () => {
       mgv._provider.pollingInterval = 250;
       const mkr_address = await OfferLogic.deploy(mgv, "SimpleMaker");
       const logic = mgv.offerLogic(mkr_address);
-      const lp = await logic.connectMarket({
+      const lp = await logic.liquidityProvider({
         base: "TokenA",
         quote: "TokenB",
         bookOptions: { maxOffers: 30 },
@@ -48,7 +48,8 @@ describe("SimpleMaker", () => {
   });
 
   describe("SimpleMaker integration tests suite", () => {
-    let lp: LP;
+    let onchain_lp: LiquidityProvider;
+    let eoa_lp: LiquidityProvider;
 
     beforeEach(async function () {
       //set mgv object
@@ -60,64 +61,111 @@ describe("SimpleMaker", () => {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       mgv._provider.pollingInterval = 250;
-      await mgv.contract["fund()"]({ value: toWei(10) });
 
       const mkr_address = await OfferLogic.deploy(mgv, "SimpleMaker");
       const logic = mgv.offerLogic(mkr_address);
-      lp = await logic.connectMarket({
+      const market = await mgv.market({
         base: "TokenA",
         quote: "TokenB",
         bookOptions: { maxOffers: 30 },
       });
+      onchain_lp = await logic.liquidityProvider(market);
+      eoa_lp = await mgv.liquidityProvider(market);
     });
 
     /* Make sure tx has been mined so we can read the result off the chain */
     const w = async (r) => (await r).wait(1);
 
     describe("Before setup", () => {
-      it("checks allowance", async () => {
-        let allowance /*:Big*/ = await lp.logic.mangroveAllowance("TokenB");
-        assert.strictEqual(allowance.toNumber(), 0, "allowance should be 0");
+      it("checks allowance for onchain logic", async () => {
+        let allowanceForLogic /*:Big*/ = await onchain_lp.mangroveAllowance(
+          "TokenB"
+        );
+        assert.strictEqual(
+          allowanceForLogic.toNumber(),
+          0,
+          "allowance should be 0"
+        );
+
         const overridesTest = { gasLimit: 100000 };
         // test specified approve amount
-        await w(lp.logic.approveMangrove("TokenB", 10 ** 9, overridesTest));
-        allowance /*:Big*/ = await lp.logic.mangroveAllowance("TokenB");
+        await w(onchain_lp.approveMangrove("TokenB", 10 ** 9, overridesTest));
+        allowanceForLogic /*:Big*/ = await onchain_lp.mangroveAllowance(
+          "TokenB"
+        );
         assert.strictEqual(
-          allowance.toNumber(),
+          allowanceForLogic.toNumber(),
           10 ** 9,
           "allowance should be 1 billion"
         );
         // test default approve amount
-        await w(lp.logic.approveMangrove("TokenB"));
-        allowance /*:Big*/ = await lp.logic.mangroveAllowance("TokenB");
+        await w(onchain_lp.approveMangrove("TokenB"));
+        allowanceForLogic /*:Big*/ = await onchain_lp.mangroveAllowance(
+          "TokenB"
+        );
         assert.strictEqual(
-          mgv.toUnits(allowance, 18).toString(),
+          mgv.toUnits(allowanceForLogic, 18).toString(),
           BigNumber.from(2).pow(256).sub(1).toString(),
           "allowance should be 2^256-1"
         );
       });
 
-      it("checks provision", async () => {
-        let balance = await mgv.balanceOf(lp.logic.address);
+      it("checks allowance for AOE provider", async () => {
+        let allowanceForEOA = await eoa_lp.mangroveAllowance("TokenB");
+        assert.strictEqual(
+          allowanceForEOA.toNumber(),
+          0,
+          "allowance should be 0"
+        );
+
+        const overridesTest = { gasLimit: 100000 };
+        // test specified approve amount
+        await w(eoa_lp.approveMangrove("TokenB", 10 ** 9, overridesTest));
+        allowanceForEOA /*:Big*/ = await eoa_lp.mangroveAllowance("TokenB");
+        assert.strictEqual(
+          allowanceForEOA.toNumber(),
+          10 ** 9,
+          "allowance should be 1 billion"
+        );
+        // test default approve amount
+        await w(eoa_lp.approveMangrove("TokenB"));
+        allowanceForEOA /*:Big*/ = await eoa_lp.mangroveAllowance("TokenB");
+        assert.strictEqual(
+          mgv.toUnits(allowanceForEOA, 18).toString(),
+          BigNumber.from(2).pow(256).sub(1).toString(),
+          "allowance should be 2^256-1"
+        );
+      });
+
+      it("checks provision for EOA provider", async () => {
+        let balance = await eoa_lp.balanceAtMangrove();
         assert.strictEqual(balance.toNumber(), 0, "balance should be 0");
-        await w(lp.logic.fundMangrove(2));
-        balance = await lp.logic.balanceAtMangrove();
+        await w(eoa_lp.fundMangrove(2));
+        balance = await eoa_lp.balanceAtMangrove();
+        assert.strictEqual(balance.toNumber(), 2, "balance should be 2");
+      });
+
+      it("checks provision for onchain logic", async () => {
+        let balance = await onchain_lp.balanceAtMangrove();
+        assert.strictEqual(balance.toNumber(), 0, "balance should be 0");
+        await w(onchain_lp.fundMangrove(2));
+        balance = await onchain_lp.balanceAtMangrove();
         assert.strictEqual(balance.toNumber(), 2, "balance should be 2");
       });
     });
 
     describe("After setup", () => {
       beforeEach(async () => {
-        await lp.logic.approveMangrove("TokenB", 10 ** 9);
+        await eoa_lp.approveMangrove("TokenB", 10 ** 9);
         //await logic.fundMangrove(10);
       });
 
       it("withdraws", async () => {
         const getBal = async () =>
           mgv._provider.getBalance(await mgv._signer.getAddress());
-        await lp.logic.fundMangrove(10);
+        await onchain_lp.fundMangrove(10);
         const oldBal = await getBal();
-        const receipt = await w(lp.logic.withdraw(10));
+        const receipt = await w(onchain_lp.withdraw(10));
         const txcost = receipt.effectiveGasPrice.mul(receipt.gasUsed);
         const diff = mgv.fromUnits(
           (await getBal()).sub(oldBal).add(txcost),
@@ -128,11 +176,11 @@ describe("SimpleMaker", () => {
       });
 
       it("pushes a new offer", async () => {
-        const provision = await lp.computeAskProvision({});
-        await lp.logic.fundMangrove(provision);
-        const { id: ofrId } = await lp.newAsk({ wants: 10, gives: 10 });
+        const provision = await onchain_lp.computeAskProvision({});
+        await onchain_lp.fundMangrove(provision);
+        const { id: ofrId } = await onchain_lp.newAsk({ wants: 10, gives: 10 });
 
-        const asks = lp.asks();
+        const asks = onchain_lp.asks();
         assert.strictEqual(
           asks.length,
           1,
@@ -142,35 +190,35 @@ describe("SimpleMaker", () => {
       });
 
       it("cancels offer", async () => {
-        const provision = await lp.computeBidProvision({});
-        await lp.logic.fundMangrove(provision);
-        const { id: ofrId } = await lp.newBid({
+        const provision = await onchain_lp.computeBidProvision({});
+        await onchain_lp.fundMangrove(provision);
+        const { id: ofrId } = await onchain_lp.newBid({
           wants: 10,
           gives: 20,
         });
 
-        await lp.cancelBid(ofrId);
+        await onchain_lp.cancelBid(ofrId);
 
-        const bids = lp.bids();
+        const bids = onchain_lp.bids();
         assert.strictEqual(bids.length, 0, "offer should have been canceled");
       });
 
       it("updates offer", async () => {
-        let provision = await lp.computeAskProvision({});
-        await lp.logic.fundMangrove(provision);
-        const { id: ofrId } = await lp.newAsk({
+        let provision = await onchain_lp.computeAskProvision({});
+        await onchain_lp.fundMangrove(provision);
+        const { id: ofrId } = await onchain_lp.newAsk({
           wants: 10,
           gives: 20,
         });
-        provision = await lp.computeAskProvision({ id: ofrId });
+        provision = await onchain_lp.computeAskProvision({ id: ofrId });
         assert.strictEqual(
           provision.toNumber(),
           0,
           `There should be no need to reprovision`
         );
-        await lp.updateAsk(ofrId, { wants: 12, gives: 10 });
+        await onchain_lp.updateAsk(ofrId, { wants: 12, gives: 10 });
 
-        const asks = lp.asks();
+        const asks = onchain_lp.asks();
         assert.strictEqual(
           asks[0].wants.toNumber(),
           12,
@@ -184,10 +232,10 @@ describe("SimpleMaker", () => {
       });
 
       it("changes gasreq", async () => {
-        await lp.logic.setDefaultGasreq(50000);
+        await onchain_lp.logic.setDefaultGasreq(50000);
         assert.strictEqual(
           50000,
-          (await lp.logic.contract.OFR_GASREQ()).toNumber(),
+          (await onchain_lp.logic.contract.OFR_GASREQ()).toNumber(),
           "Offer default gasreq not updated"
         );
       });
