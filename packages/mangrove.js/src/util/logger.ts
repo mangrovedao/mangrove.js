863984e0a5c5e06a2f54edba0bc518bdc5fa58c8
AK <abdelkader@mangrove.exchange>
2022-03-07 23:58:13 +0100

    chore(mangrove.js/logging): clean code

--- packages/mangrove.js/src/util/logger.ts
+++ packages/mangrove.js/src/util/logger.ts
@@ -1,68 +1,18 @@
 // FIXME: The logger has been stunted by removing the dependency to commonlib.js
-//        as a temporary workaround for issue #220.
+//        as a temporary workaround for issue #220 and issue #226.
 //        To avoid reverting a merge commit and the burden of maintaining that old feature branch,
 //        we have opted to keep most of the logging code but only support simplified logging to console.
 //
 //        For references on why we want to avoid revert merge commits:
 //          Long (Linus): https://github.com/git/git/blob/master/Documentation/howto/revert-a-faulty-merge.txt
 //          Short: https://www.datree.io/resources/git-undo-merge
-// import {
-//   createLogger,
-//   BetterLogger,
-//   format,
-//   transports,
-//   logdataLimiter,
-// } from "@mangrovedao/commonlib-js";
-// import os from "os";
-import inspect from "object-inspect";
-// import config from "./config";
 
-// const consoleLogFormat = format.printf(
-//   ({ level, message, timestamp, ...metadata }) => {
-//     let msg = `${timestamp} [${level}] `;
-//     if (metadata.contextInfo) {
-//       msg += `[${metadata.contextInfo}] `;
-//     }
-//     msg += message;
-//     if (metadata.data !== undefined) {
-//       msg += ` | data: ${stringifyData(metadata.data)}`;
-//     }
-//     if (metadata.stack) {
-//       msg += `${os.EOL}${metadata.stack}`;
-//     }
-//     return msg;
-//   }
-// );
+import inspect from "object-inspect";
 
 const stringifyData = (data) => {
   if (typeof data == "string") return data;
   else return inspect(data);
 };
-
-// const defaultLogLevel = "error";
-
-// FIXME : The config module is not compatible with browser
-// const logLevel = config.MangroveJs.has("logLevel")
-//   ? config.MangroveJs.get<string>("logLevel")
-//   : defaultLogLevel;
-// const logLevel = defaultLogLevel;
-
-// const additionalTransports = [];
-
-// if (config.MangroveJs.has("logFile")) {
-//   const logFile = config.MangroveJs.get<string>("logFile");
-//   additionalTransports.push(
-//     new transports.File({
-//       level: logLevel,
-//       filename: logFile,
-//       format: format.combine(
-//         format.splat(),
-//         format.timestamp(),
-//         consoleLogFormat
-//       ),
-//     })
-//   );
-// }
 
 // FIXME: Temporary copy until issue #220 is fixed
 export const logdataLimiter = (data: Record<string, any>): any => {
@@ -78,10 +28,5 @@ export const logger = {
     console.warn(msg + " " + stringifyData(data));
   },
 };
-// export const logger: BetterLogger = createLogger(
-//   consoleLogFormat,
-//   logLevel,
-//   additionalTransports
-// );
 
 export default logger;
