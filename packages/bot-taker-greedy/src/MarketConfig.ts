6a7d4227877a323e586d8ba95b8e7e9d6739bce2
Espen Højsgaard <espen@espen.dk>
2022-03-30 13:36:26 +0200

    feat!: Reduce bot-taker-greedy to only taker functionality

--- packages/bot-taker-greedy/src/MarketConfig.ts
+++ packages/bot-taker-greedy/src/MarketConfig.ts
@@ -1,11 +1,3 @@
-export type MakerConfig = {
-  offerRate: number;
-  bidProbability: number;
-  lambda: number;
-  maxQuantity: number;
-  maxTotalLiquidityPublished: number;
-};
-
 export type TakerConfig = {
   targetAllowance: number;
   takeRate: number;
@@ -16,6 +8,5 @@ export type TakerConfig = {
 export type MarketConfig = {
   baseToken: string;
   quoteToken: string;
-  makerConfig: MakerConfig;
   takerConfig: TakerConfig;
 };
