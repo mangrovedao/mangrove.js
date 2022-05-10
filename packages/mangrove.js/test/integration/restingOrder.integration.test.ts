d3fce82260c33ed78e865bb18436a407e6dc2dd5
JeanKa <jean@mangrove.exchange>
2022-05-10 15:44:28 +0200

    testing time to live for resting orders

--- packages/mangrove.js/test/integration/restingOrder.integration.test.ts
+++ packages/mangrove.js/test/integration/restingOrder.integration.test.ts
@@ -1,7 +1,9 @@
 // Integration tests for SimpleMaker.ts
 import { afterEach, beforeEach, describe, it } from "mocha";
+const chalk = require("chalk");
 
-import { BigNumber, utils } from "ethers";
+import { utils } from "ethers";
+const { ethers } = require("hardhat");
 
 import assert from "assert";
 import { Mangrove, OfferLogic, LiquidityProvider, Market } from "../../src";
@@ -12,6 +14,13 @@ import { Big } from "big.js";
 Big.prototype[Symbol.for("nodejs.util.inspect.custom")] = function () {
   return `<Big>${this.toString()}`; // previously just Big.prototype.toString;
 };
+
+// advance block numbers
+async function mineNBlocks(n: number) {
+  for (let index = 0; index < n; index++) {
+    await ethers.provider.send("evm_mine", []);
+  }
+}
 
 describe("RestingOrder", () => {
   let mgv: Mangrove;
@@ -70,12 +79,16 @@ describe("RestingOrder", () => {
 
       // minting As and Bs for test runner
       const me = await mgv._signer.getAddress();
-      await mgv.token("TokenA").contract.mint(me, utils.parseUnits("100", 18));
+      await w(
+        mgv.token("TokenA").contract.mint(me, utils.parseUnits("100", 18))
+      );
       // depositing tokens on the strat (approve and deposit)
-      await mgv.token("TokenA").approve(orderContract.logic.address);
+      await w(mgv.token("TokenA").approve(orderContract.logic.address));
       await w(orderContract.logic.depositToken("TokenA", 50));
 
-      await mgv.token("TokenB").contract.mint(me, utils.parseUnits("100", 18));
+      await w(
+        mgv.token("TokenB").contract.mint(me, utils.parseUnits("100", 18))
+      );
 
       // `me` proposes asks on Mangrove so should approve base
       await w(mgv.token("TokenA").approveMangrove());
@@ -117,6 +130,47 @@ describe("RestingOrder", () => {
         "Order should have been partially filled"
       );
       assert(orderResult.penalty.eq(0), "No offer should have failed");
+    });
+
+    it("resting order with deadline", async () => {
+      const orderContractAsLP: LiquidityProvider = orderContract;
+      const provision = await orderContractAsLP.computeBidProvision();
+      const market: Market = orderContract.market;
+      // `me` buying base so should approve orderContract for quote
+      await w(mgv.token("TokenB").approve(orderContract.logic.address));
+
+      const orderResult: Market.OrderResult = await market.buy({
+        wants: 20, // tokenA
+        gives: 20, // tokenB
+        restingOrder: { provision: provision, blocksToLiveForRestingOrder: 5 },
+      });
+
+      assert(orderResult.offerId > 0, "Resting order was not posted");
+      const ttl = await mgv.orderContract.expiring(
+        mgv.token("TokenB").address,
+        mgv.token("TokenA").address,
+        orderResult.offerId
+      );
+      console.log(ttl.toString());
+
+      // taking resting offer
+
+      await w(mgv.token("TokenB").approveMangrove());
+      await w(mgv.token("TokenA").approveMangrove());
+
+      const result = await market.sell({ wants: 5, gives: 5 });
+      assert(result.got.eq(5), "Sell order went wrong");
+      assert(
+        await orderContract.market.isLive("bids", orderResult.offerId),
+        "Residual should still be in the book"
+      );
+      mineNBlocks(6);
+      assert(
+        (await mgv._provider.getBlockNumber()) > ttl.toNumber(),
+        "Block number is incorrect"
+      );
+      const result_ = await market.sell({ wants: 5, gives: 5 });
+      assert(result_.penalty.gt(0), "Order should have reneged");
     });
   });
 });
