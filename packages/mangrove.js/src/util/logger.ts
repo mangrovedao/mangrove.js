e59e42537f86364217455032cbf80063630cfe8c
adhusson <adhusson@gmail.com>
2022-05-31 17:29:53 +0200

    feat: reenable logging enable/disabled, logdatalimiter

--- packages/mangrove.js/src/util/logger.ts
+++ packages/mangrove.js/src/util/logger.ts
@@ -1,5 +1,10 @@
 import inspect from "object-inspect";
-import { createLogger, CommonLogger, format } from "@mangrovedao/commonlib.js";
+import {
+  createLogger,
+  CommonLogger,
+  format,
+  logdataLimiter,
+} from "@mangrovedao/commonlib.js";
 import os from "os";
 
 const stringifyData = (data) => {
@@ -7,13 +12,7 @@ const stringifyData = (data) => {
   else return inspect(data);
 };
 
-// FIXME: Temporary copy until issue #220 is fixed
-export const logdataLimiter = (data: Record<string, any>): any => {
-  return inspect(data, { maxStringLength: 1000 });
-};
-
-// FIXME: Temporary dumb toggle until issue #220 is fixed
-let loggingEnabled = true;
+let loggingEnabled = false;
 export function enableLogging(): void {
   loggingEnabled = true;
 }
@@ -21,9 +20,9 @@ export function disableLogging(): void {
   loggingEnabled = false;
 }
 
-const consoleLogFormat = format.printf(
-  ({ level, message, timestamp, ...metadata }) => {
-    console.log("in there");
+const consoleLogFormat = format.combine(
+  format((info) => loggingEnabled && info)(),
+  format.printf(({ level, message, timestamp, ...metadata }) => {
     let msg = `${timestamp} [${level}] `;
     if (metadata.contextInfo !== undefined) {
       msg += `[${metadata.contextInfo}] `;
@@ -36,10 +35,11 @@ const consoleLogFormat = format.printf(
       msg += `${os.EOL}${metadata.stack}`;
     }
     return msg;
-  }
+  })
 );
 
 const logLevel = "debug";
 export const logger: CommonLogger = createLogger(consoleLogFormat, logLevel);
 
 export default logger;
+export { logdataLimiter };
