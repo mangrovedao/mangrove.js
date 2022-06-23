a9991389a2c6274406a98f14b134f82e157bf057
Arthur Bensimon <arthur.bensimon@gmail.com>
2022-06-23 16:13:05 +0200

    configurable multi markets

--- packages/bot-arbitrage/src/index.ts
+++ packages/bot-arbitrage/src/index.ts
@@ -4,13 +4,17 @@
  * @module
  */
 
-// import { logger } from "./util/logger";
+import http from "http";
+import finalhandler from "finalhandler";
+import serveStatic from "serve-static";
 import { BotArbitrage } from "./BotArbitrage";
 import { WebSocketProvider } from "@ethersproject/providers";
 import { NonceManager } from "@ethersproject/experimental";
 import { Wallet } from "@ethersproject/wallet";
 import { ethers } from "ethers";
 import dotenvFlow from "dotenv-flow";
+import { Mangrove } from "@mangrovedao/mangrove.js";
+import markets from "../config/markets.json";
 dotenvFlow.config();
 
 const mgvMultiOrderAbi =
@@ -27,54 +31,69 @@ const main = async () => {
   if (!process.env["PRIVATE_KEY"])
     throw new Error("No private key provided in PRIVATE_KEY");
 
-  if (!process.env["MANGROVE_CONTRACT_ADDRESS"])
-    throw new Error(
-      "No address provided for the mangrove contract. Check your .env.local."
-    );
-  if (
-    !process.env["OUTBOUND_TOKEN_ADDRESS"] ||
-    !process.env["INBOUND_TOKEN_ADDRESS"]
-  )
-    throw new Error(
-      "No market base &or quote provided in OUTBOUND_TOKEN_ADDRESS, INBOUND_TOKEN_ADDRESS"
-    );
   if (!process.env["MULTI_ORDER_CONTRACT_ADDRESS"])
-    throw new Error(
-      "No address provided for the multi order contract. Check your .env.local."
-    );
+    throw new Error("No address provided in MULTI_ORDER_CONTRACT_ADDRESS");
 
-  const provider = new WebSocketProvider(process.env["ETHEREUM_NODE_URL"]);
-  const blocksSubscriber = new WebSocketProvider(
-    process.env["ETHEREUM_NODE_URL"]
-  );
-  const signer = new Wallet(process.env["PRIVATE_KEY"], provider);
-  const nonceManager = new NonceManager(signer);
+  const NODE_URL = process.env["ETHEREUM_NODE_URL"];
+  const PRIV_KEY = process.env["PRIVATE_KEY"];
+  const MULTI_ADDRESS = process.env["MULTI_ORDER_CONTRACT_ADDRESS"];
 
-  const mgvContract = new ethers.Contract(
-    process.env["MANGROVE_CONTRACT_ADDRESS"],
-    mgvAbi,
-    nonceManager
-  );
-  const mgvMultiOrderContract = new ethers.Contract(
-    process.env["MULTI_ORDER_CONTRACT_ADDRESS"],
-    mgvMultiOrderAbi,
-    nonceManager
-  );
+  const mgv = await Mangrove.connect(process.env["ETHEREUM_NODE_URL"]);
 
-  try {
-    const simpleArbitrageBot = new BotArbitrage(
-      mgvContract,
-      mgvMultiOrderContract,
-      blocksSubscriber,
-      process.env["OUTBOUND_TOKEN_ADDRESS"],
-      process.env["INBOUND_TOKEN_ADDRESS"]
-    );
-    await simpleArbitrageBot.start();
-  } catch (error) {
-    console.error(error);
-  } finally {
-  }
+  let runningBots = [];
+
+  // outbound = base
+  // inbound = quote
+  markets.markets.forEach((mkt) => {
+    try {
+      const base = mgv.token(mkt.base).address;
+      const quote = mgv.token(mkt.quote).address;
+      if (base.length != 42 || quote.length != 42) {
+        throw new Error("Base or quote address not good");
+      }
+
+      const provider = new WebSocketProvider(NODE_URL);
+      const blocksSubscriber = new WebSocketProvider(NODE_URL);
+      const signer = new Wallet(PRIV_KEY, provider);
+      const nonceManager = new NonceManager(signer);
+      const mgvContract = new ethers.Contract(
+        mgv._address,
+        mgvAbi,
+        nonceManager
+      );
+      const mgvMultiOrderContract = new ethers.Contract(
+        MULTI_ADDRESS,
+        mgvMultiOrderAbi,
+        nonceManager
+      );
+      const simpleArbitrageBot = new BotArbitrage(
+        mgvContract,
+        mgvMultiOrderContract,
+        blocksSubscriber,
+        base,
+        quote
+      );
+      simpleArbitrageBot.start();
+      runningBots.push(simpleArbitrageBot);
+    } catch (error) {
+      console.error(error);
+    } finally {
+    }
+  });
+  mgv.disconnect();
 };
+
+// The node http server is used solely to serve static information files for environment management
+const staticBasePath = "./static";
+
+const serve = serveStatic(staticBasePath, { index: false });
+
+const server = http.createServer(function (req, res) {
+  const done = finalhandler(req, res);
+  serve(req, res, () => done(undefined)); // 'undefined' means no error
+});
+
+server.listen(process.env.PORT || 8080);
 
 function logErrorAndExit(err: Error) {
   console.error(err);
@@ -91,15 +110,3 @@ main()
   .catch((e) => {
     logErrorAndExit(e);
   });
-
-// The node http server is used solely to serve static information files for environment management
-// const staticBasePath = "./static";
-
-// const serve = serveStatic(staticBasePath, { index: false });
-
-// const server = http.createServer(function (req, res) {
-//   const done = finalhandler(req, res);
-//   serve(req, res, () => done(undefined)); // 'undefined' means no error
-// });
-
-// server.listen(process.env.PORT || 8080);
