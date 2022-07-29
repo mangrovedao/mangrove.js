c15de3d8d2eb28c49dd2f0b22d652647fb5f9c74
Adrien Husson <adhusson@gmail.com>
2022-07-29 11:59:46 +0200

    fix(bot-template): no more hardhat

--- packages/bot-template/test/integration/templateBot.integration.tests.ts
+++ packages/bot-template/test/integration/templateBot.integration.tests.ts
@@ -9,39 +9,43 @@ import chaiAsPromised from "chai-as-promised";
 chai.use(chaiAsPromised);
 import Mangrove from "@mangrovedao/mangrove.js";
 import { TemplateBot } from "../../src/TemplateBot";
-import * as hre from "hardhat";
-import "hardhat-deploy-ethers/dist/src/type-extensions";
+// import * as hre from "hardhat";
+// import "hardhat-deploy-ethers/dist/src/type-extensions";
+import { Signer, ethers } from "ethers";
 import { config } from "../../src/util/config";
-import { SignerWithAddress } from "hardhat-deploy-ethers/dist/src/signers";
+// import { SignerWithAddress } from "hardhat-deploy-ethers/dist/src/signers";
 
 // TODO: Basic can-connect test - delete/update as needed
 describe("Can connect to Mangrove on local chain", () => {
   it("should be able to connect to Mangrove", function () {
     return expect(
-      Mangrove.connect({ provider: this.test?.parent?.parent?.ctx.provider })
+      Mangrove.connect({
+        provider: this.server.url,
+        privateKey: this.accounts.tester.key,
+      })
     ).to.eventually.be.fulfilled;
   });
 });
 
 // TODO: integration tests - add/update as needed
 describe("Bot integration tests", () => {
-  let botSigner: SignerWithAddress;
+  let botSigner: Signer;
   let mgv: Mangrove;
 
-  before(async function () {
+  before(async function () {});
+
+  beforeEach(async function () {
+    // must be in beforeeach or resets will disrupt nonce mgmt
+    botSigner = new ethers.Wallet(
+      this.accounts.deployer.key,
+      new ethers.providers.JsonRpcProvider(this.server.url)
+    );
     //TODO:
     // update "gasUpdater" below to be the named address (see packages/hardhat-utils/config/hardhat-mangrove-config.js)
     // specific to this bot
-    botSigner = await hre.ethers.getNamedSigner("gasUpdater");
-  });
-
-  beforeEach(async function () {
     mgv = await Mangrove.connect({
-      //provider: this.test?.parent?.parent?.ctx.provider,
       signer: botSigner,
     });
-
-    const deployer = (await hre.ethers.getNamedSigners()).deployer;
   });
 
   afterEach(() => {
