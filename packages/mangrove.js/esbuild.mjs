14a12b181f570418d527456d90252ebb426f13be
lnist <16150076+lnist@users.noreply.github.com>
2022-08-25 16:30:30 +0200

    fix(build): lint mjs files in mangrove.js

--- packages/mangrove.js/esbuild.mjs
+++ packages/mangrove.js/esbuild.mjs
@@ -5,18 +5,18 @@ import { build } from "esbuild";
 import resolve from "esbuild-plugin-resolve";
 
 build({
-    entryPoints: ['./dist/nodejs/index.js'],
-    bundle: true,
-    minify: true,
-    outfile: BrowserBuildPath,
-    platform: 'browser',
-    format: 'cjs',
-    // FIXME either polyfill or create a browser-build that does not embark node.ts e.g. through a browser-index.ts
-    external: ['path','child_process','fs','os','util','url','assert'],
-    plugins: [
-        resolve({
-        "@mangrovedao/commonlib.js": "../../../shims/commonlib.ts",
-        "./util/readJsonWallet": "../../shims/readJsonWallet.ts",
-        }),
-    ],
+  entryPoints: ["./dist/nodejs/index.js"],
+  bundle: true,
+  minify: true,
+  outfile: BrowserBuildPath,
+  platform: "browser",
+  format: "cjs",
+  // FIXME either polyfill or create a browser-build that does not embark node.ts e.g. through a browser-index.ts
+  external: ["path", "child_process", "fs", "os", "util", "url", "assert"],
+  plugins: [
+    resolve({
+      "@mangrovedao/commonlib.js": "../../../shims/commonlib.ts",
+      "./util/readJsonWallet": "../../shims/readJsonWallet.ts",
+    }),
+  ],
 });
