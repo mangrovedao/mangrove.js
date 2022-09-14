900475fb490f7258549ef96435c583cf3b6a9191
Adrien Husson <adhusson@gmail.com>
2022-09-14 18:28:28 +0200

    feat: deployed contracts are written to addresses/<network>.json

---
+++ packages/mangrove-solidity/copyAddresses.js
@@ -0,0 +1,15 @@
+/* ***** Mangrove tooling script ********* *
+This script copies foundry broadcasts to the distribution directory, according
+to the config file.
+*/
+
+const path = require("path");
+const shell = require("shelljs");
+shell.config.fatal = true; // throw if a command errors
+
+const sourceDir = path.resolve("./addresses");
+const outDir = path.resolve("./dist/addresses");
+
+shell.cd(sourceDir);
+shell.mkdir("-p", outDir);
+shell.cp("*", outDir);
