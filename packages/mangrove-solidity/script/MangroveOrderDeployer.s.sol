ed478a7265c06ed8d4c0008f495121e0aa6e96ef
lnist <16150076+lnist@users.noreply.github.com>
2022-09-21 13:38:04 +0200

    fix(deploy): Consequential changes from master merge

--- packages/mangrove-solidity/script/MangroveOrderDeployer.s.sol
+++ packages/mangrove-solidity/script/MangroveOrderDeployer.s.sol
@@ -5,28 +5,27 @@ import {Script, console} from "forge-std/Script.sol";
 import {MangroveOrderEnriched, IERC20, IMangrove} from "mgv_src/periphery/MangroveOrderEnriched.sol";
 import {Deployer} from "./lib/Deployer.sol";
 
-/** @notice deploys a MangroveOrder instance */
-// forge script --fork-url $MUMBAI_NODE_URL \
-// --private-key $MUMBAI_DEPLOYER_PRIVATE_KEY \
-// --sig "run(address)" \
-// --etherscan-api-key $POLYGONSCAN_API \
-// --verify \
-// MangroveOrderDeployer \
-// $MUMBAI_TESTER_ADDRESS
-
+/** @notice deploys a MangroveOrder instance
+  ADMIN=$MUMBAI_TESTER_ADDRESS forge script --fork-url $MUMBAI_NODE_URL \
+  --private-key $MUMBAI_TESTER_PRIVATE_KEY \
+  --broadcast \
+  --verify
+  MangroveOrderDeployer
+*/
 contract MangroveOrderDeployer is Deployer {
+  function run() public {
+    innerRun({admin: vm.envAddress("ADMIN")});
+  }
+
   /**
-  @param admin address of the adim on Mango after deployment 
+  @param admin address of the admin on Mango after deployment 
   */
-  function run(address admin) public {
+  function innerRun(address admin) public {
     console.log("Deploying Mangrove Order...");
-    (address $mgv,) = ens.get("Mangrove");
+    IMangrove mgv = IMangrove(fork.get("Mangrove"));
     vm.broadcast();
-    MangroveOrderEnriched mgv_order = new MangroveOrderEnriched(
-      IMangrove(payable($mgv)),
-      admin
-    );
-    ens.set("MangroveOrderEnriched", address(mgv_order), false);
+    MangroveOrderEnriched mgv_order = new MangroveOrderEnriched(mgv, admin);
+    fork.set("MangroveOrderEnriched", address(mgv_order), false);
     outputDeployment();
     console.log("Deployed!", address(mgv_order));
   }
