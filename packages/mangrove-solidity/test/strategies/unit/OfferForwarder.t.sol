c64bf4d79d238a22b8782fae7aa4609b51d0d5ef
jkrivine <jean@mangrove.exchange>
2022-09-21 15:10:00 +0200

    more tests

--- packages/mangrove-solidity/test/strategies/unit/OfferForwarder.t.sol
+++ packages/mangrove-solidity/test/strategies/unit/OfferForwarder.t.sol
@@ -17,7 +17,7 @@ contract OfferForwarderTest is OfferLogicTest {
     weth.approve(address(makerContract.router()), type(uint).max);
   }
 
-  function test_makerProvisionsAreLocked(uint fund) public {
+  function test_derivedGaspriceIsAccurateEnough(uint fund) public {
     vm.assume(
       fund >= makerContract.getMissingProvision(weth, usdc, type(uint).max, 0, 0)
     );
@@ -51,10 +51,81 @@ contract OfferForwarderTest is OfferLogicTest {
     // checking that not storing `loss_for_maker` in `ownerData` saves more maker funds than actually storing it.
     // currently we are storing 0 at a cost of 5000 g.u. Storing loss_for_maker would cost an additional 15K g.u
     // we use mangrove gasprice to evaluate this.
-    console.log(loss_for_maker);
+    console.log("counterexample:", loss_for_maker);
     assertTrue(
       loss_for_maker < (15000 * global.gasprice() * 10**9), 
       "rounding exceeds storage write cost"
     );
   }
+
+  function test_updateOfferWithFundsUpdatesGasprice() public {
+    vm.prank(maker);
+    uint offerId = makerContract.newOffer{value: 0.1 ether}({
+      outbound_tkn: weth,
+      inbound_tkn: usdc,
+      wants: 2000 * 10**6,
+      gives: 1 ether,
+      gasreq: type(uint).max,
+      gasprice: 0,
+      pivotId: 0
+    });
+    uint old_gasprice = mgv
+      .offerDetails(address(weth), address(usdc), offerId)
+      .gasprice();
+    vm.prank(maker);
+    makerContract.updateOffer{value: 0.2 ether}({
+      outbound_tkn: weth,
+      inbound_tkn: usdc,
+      wants: 2000 * 10**6,
+      gives: 1 ether,
+      gasreq: type(uint).max,
+      gasprice: 0,
+      pivotId: 0,
+      offerId: offerId
+    });
+    assertTrue(old_gasprice < mgv
+      .offerDetails(address(weth), address(usdc), offerId)
+      .gasprice(), "Gasprice not updated as expected");
+  }
+
+  function test_failedOfferCreditsOwner(uint fund) public {
+    vm.assume(
+      fund >= makerContract.getMissingProvision(weth, usdc, type(uint).max, 0, 0)
+    );
+    vm.assume(fund < 5 ether);
+    vm.startPrank(maker);
+    uint offerId = makerContract.newOffer{value: fund}({
+      outbound_tkn: weth,
+      inbound_tkn: usdc,
+      wants: 2000 * 10**6,
+      gives: 1 ether,
+      gasreq: type(uint).max,
+      gasprice: 0,
+      pivotId: 0
+    });
+    // revoking Mangrove's approvals to make `offerId` fail
+    makerContract.approve(weth, address(mgv), 0);
+    vm.stopPrank();
+    uint provision = makerContract.provisionOf(weth,usdc,offerId);
+    // taker has approved mangrove in the setUp
+    vm.startPrank(taker);
+    (uint takergot, , uint bounty,) = mgv.marketOrder({
+      outbound_tkn: address(weth),
+      inbound_tkn: address(usdc),
+      takerWants: 0.5 ether,
+      takerGives: cash(usdc, 1000),
+      fillWants: true
+    });
+    vm.stopPrank();
+    assertTrue(bounty > 0 && takergot == 0, "trade should have failed");
+    uint provision_after_fail = makerContract.provisionOf(weth,usdc,offerId);
+    // checking that approx is small in front a storage write (approx < write_cost / 10)
+    (Global.t global,) = mgv.config($(weth),$(usdc));
+    uint approx_cost = (provision - bounty) - provision_after_fail ;
+    assertTrue(
+      approx_cost < 1_000 * global.gasprice() * 10**9,
+      "Approximation of offer owner's credit is too coarse"
+    );
+  }
+
 }
