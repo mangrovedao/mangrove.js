03bad92e95dc756090d7ce274bc960697b9b4ddb
lnist <16150076+lnist@users.noreply.github.com>
2022-09-23 12:28:43 +0200

    Fix comment

--- packages/mangrove-solidity/src/strategies/interfaces/IOrderLogic.sol
+++ packages/mangrove-solidity/src/strategies/interfaces/IOrderLogic.sol
@@ -64,7 +64,6 @@ interface IOrderLogic {
   ///@param takerGot How much the taker got
   ///@param takerGave How much the taker gave
   ///@param penalty How much penalty was given
-  ///@param restingOrderId The Id of the resting order
   event OrderSummary(
     IMangrove mangrove,
     IERC20 indexed outbound_tkn,
