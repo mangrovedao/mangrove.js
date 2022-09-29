1a49dfe4a616010fd4ebba217bb7670ff6874cf5
Adrien Husson <adhusson@gmail.com>
2022-09-29 18:01:07 +0200

    fix pp files

--- packages/mangrove-solidity/src/preprocessed/MgvStructs.post.sol
+++ packages/mangrove-solidity/src/preprocessed/MgvStructs.post.sol
@@ -18,6 +18,8 @@ pragma solidity ^0.8.13;
             GENERATED FILE. DO NOT EDIT.
  * ************************************************** */
 
+// Note: can't do Type.Unpacked because typechain mixes up multiple 'Unpacked' structs under different namespaces. So for consistency we don't do Type.Packed either. We do TypeUnpacked and TypePacked.
+
 import {OfferPacked, OfferUnpacked} from "./MgvOffer.post.sol";
 import "./MgvOffer.post.sol" as Offer;
 import {OfferDetailPacked, OfferDetailUnpacked} from "./MgvOfferDetail.post.sol";
