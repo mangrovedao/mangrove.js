5e02a714d5a54e8f562471182577d2a7fd7ffbe8
lnist <16150076+lnist@users.noreply.github.com>
2022-10-03 12:42:12 +0200

    fix(restingOrder): wrong decimals for resting orders
    OfferWriteEvent-parsing did not use the right inbound/outbound.

--- packages/mangrove.js/test/integration/restingOrder.integration.test.ts
+++ packages/mangrove.js/test/integration/restingOrder.integration.test.ts
@@ -155,6 +155,8 @@ describe("RestingOrder", () => {
 
       assert(orderResult.restingOrder.volume.eq(10));
       assert(orderResult.restingOrder.price.eq(1));
+      assert(orderResult.restingOrder.wants.eq(10));
+      assert(orderResult.restingOrder.gives.eq(10));
 
       // taking resting offer
 
