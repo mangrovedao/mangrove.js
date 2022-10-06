842dcd8f5bf0c74cc8387f286fb02efd7593f168
Adrien Husson <adhusson@gmail.com>
2022-10-06 22:03:27 +0200

    refactor(mangrove.js): don't reexport addresses/abis

--- packages/mangrove.js/src/constants.ts
+++ packages/mangrove.js/src/constants.ts
@@ -1,11 +1,22 @@
-import addressesPerNetwork from "./constants/addresses.json";
+import knownAddresses from "./constants/addresses.json";
 import tokenDecimals from "./constants/tokenDecimals.json";
 import tokenDisplayedDecimals from "./constants/tokenDisplayedDecimals.json";
 import tokenDisplayedAsPriceDecimals from "./constants/tokenDisplayedAsPriceDecimals.json";
 
-export const addresses = {
-  ...addressesPerNetwork,
-};
+import mgvCore from "@mangrovedao/mangrove-core";
+
+// Merge known addresses and addresses provided by mangrove-core, with priority to mangrove-core addresses.
+
+const addresses = { ...knownAddresses };
+
+for (const [network, networkAddresses] of Object.entries(mgvCore.addresses)) {
+  addresses[network] ??= {};
+  for (const { name, address } of networkAddresses as any) {
+    addresses[network][name] = address;
+  }
+}
+
+export { addresses };
 
 export const EOA_offer_gasreq = 5000;
 
