f0bdd04f0953024831c50f0b1c0cdc0daf1ea61d
Peter <105609241+peterMangrove@users.noreply.github.com>
2022-11-11 14:06:22 +0100

    doc(sdk): updated snipe tutorial to a how to (#858)

--- packages/mangrove.js/examples/how-tos/snipe-offer.js
+++ packages/mangrove.js/examples/how-tos/snipe-offer.js
@@ -5,8 +5,8 @@ var parsed = require("dotenv").config();
 const { Mangrove, ethers } = require("@mangrovedao/mangrove.js");
 
 // Create a wallet with a provider to interact with the chain.
-const provider = new ethers.providers.WebSocketProvider(process.env.RPC_URL); // For real chain use
-// const provider = new ethers.providers.WebSocketProvider(process.env.LOCAL_URL); // For local chain use
+// const provider = new ethers.providers.WebSocketProvider(process.env.RPC_URL); // For real chain use
+const provider = new ethers.providers.WebSocketProvider(process.env.LOCAL_URL); // For local chain use
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider); // Use either own account or if on local chain use an anvil account
 
 // Connect the API to Mangrove
@@ -23,26 +23,33 @@ const market = await mgv.market({ base: "DAI", quote: "USDC" });
 // Check it's live, should display the best asks of the DAI, USDC market
 market.consoleAsks();
 
-await mgv.approveMangrove("USDC");
+// Make sure to use the correct offerID
+let offerId = 5572;
 
-let asks = await market.getSemibook("asks");
-let offerId = asks.getBestInCache();
-let offer = await asks.offerInfo(offerId);
+// Get all the info about the offer
+let offer = await market.getSemibook("asks").offerInfo(offerId);
 
+// Log offer to see what data in holds
 console.log(offer);
 
+// Approve Mangrove to take USDC from your account
+await mgv.approveMangrove("USDC");
+
+// Snipe the offer using the information about the offer.
 let result = await market.snipe({
   targets: [
     {
       offerId: offer.id,
       takerWants: offer.gives,
       takerGives: offer.wants,
-      gasLimit: "9999999999", // makes it faster, should be fixed.
+      // gasLimit: offer.gasreq, // not mandatory
     },
   ],
   ba: "asks",
 });
 
+// Log the result of snipe
 console.log(result);
 
+// Log asks to see that the offer is gone.
 market.consoleAsks();
