407e45df22f855f83b1924f8efe2d3b7a4e3dbd5
Adrien Husson <adhusson@gmail.com>
2022-11-23 19:09:27 +0100

    fix: rename pipeOut to pipe

--- packages/mangrove.js/src/util/test/mochaHooks.ts
+++ packages/mangrove.js/src/util/test/mochaHooks.ts
@@ -8,7 +8,7 @@ import ProxyServer from "transparent-proxy";
 const serverParams = {
   host: "127.0.0.1",
   port: 8545, // use 8545 for the actual node, but let all connections go through proxies to be able to cut the connection before snapshot revert.
-  pipeOut: false,
+  pipe: false,
   script: "MangroveJsDeploy",
 };
 
