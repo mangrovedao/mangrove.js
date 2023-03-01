18ccbc4065fcf03d68a0a47627d148672ec11832
Peter Kejlberg Madsen <peter@mangrove.exchange>
2023-03-01 15:06:35 +0100

    correct build

--- packages/bot-arbitrage/src/uniswap/libs/constants.ts
+++ packages/bot-arbitrage/src/uniswap/libs/constants.ts
@@ -1,7 +1,5 @@
 // This file stores web3 related constants such as addresses, token definitions, ETH currency references and ABI's
 
-import { SupportedChainId, Token } from "@uniswap/sdk-core";
-
 // Addresses
 
 export const POOL_FACTORY_CONTRACT_ADDRESS =
@@ -13,17 +11,3 @@ export const SWAP_ROUTER_ADDRESS = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
 
 export const MAX_FEE_PER_GAS = 100000000000;
 export const MAX_PRIORITY_FEE_PER_GAS = 100000000000;
-
-// Currencies and Tokens
-
-export const WETH_TOKEN = new Token(
-  SupportedChainId.POLYGON,
-  "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
-  18
-);
-
-export const USDC_TOKEN = new Token(
-  SupportedChainId.POLYGON,
-  "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
-  6
-);
