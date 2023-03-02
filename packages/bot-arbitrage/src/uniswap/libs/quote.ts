8afa3312e2d76063eb8d74f2234db785a6f7c5e7
Peter Kejlberg Madsen <peter@mangrove.exchange>
2023-03-02 15:50:05 +0100

    remove submodule

--- packages/bot-arbitrage/src/uniswap/libs/quote.ts
+++ packages/bot-arbitrage/src/uniswap/libs/quote.ts
@@ -1,6 +1,6 @@
-import IUniswapV3Factory from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Factory.sol/IUniswapV3Factory.json";
-import IUniswapV3PoolABI from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json";
-import Quoter from "@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json";
+// import IUniswapV3Factory from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Factory.sol/IUniswapV3Factory.json";
+// import IUniswapV3PoolABI from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json";
+// import Quoter from "@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json";
 import { BigNumber, Signer, ethers, providers } from "ethers";
 import {
   MAX_FEE_PER_GAS,
@@ -22,21 +22,22 @@ export async function quote(params: {
   fee: number;
   provider: providers.Provider;
 }): Promise<BigNumber> {
-  const quoterContract = new ethers.Contract(
-    QUOTER_CONTRACT_ADDRESS,
-    Quoter.abi,
-    params.provider
-  );
-  const amounIn = params.amountIn.toString();
-  const quotedAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
-    params.in,
-    params.out,
-    params.fee,
-    amounIn,
-    0
-  );
+  // const quoterContract = new ethers.Contract(
+  //   QUOTER_CONTRACT_ADDRESS,
+  //   Quoter.abi,
+  //   params.provider
+  // );
+  // const amounIn = params.amountIn.toString();
+  // const quotedAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
+  //   params.in,
+  //   params.out,
+  //   params.fee,
+  //   amounIn,
+  //   0
+  // );
 
-  return quotedAmountOut;
+  // return quotedAmountOut;
+  return BigNumber.from("0");
 }
 
 export async function getPoolContract(params: {
@@ -44,22 +45,23 @@ export async function getPoolContract(params: {
   out: string;
   fee: number;
   provider: providers.Provider;
-}): Promise<ethers.Contract> {
-  const poolFactoryContract = new ethers.Contract(
-    POOL_FACTORY_CONTRACT_ADDRESS,
-    IUniswapV3Factory.abi,
-    params.provider
-  );
+}): Promise<ethers.Contract> | undefined {
+  // const poolFactoryContract = new ethers.Contract(
+  //   POOL_FACTORY_CONTRACT_ADDRESS,
+  //   IUniswapV3Factory.abi,
+  //   params.provider
+  // );
 
-  const currentPoolAddress = await poolFactoryContract.callStatic.getPool(
-    params.in,
-    params.out,
-    params.fee
-  );
+  // const currentPoolAddress = await poolFactoryContract.callStatic.getPool(
+  //   params.in,
+  //   params.out,
+  //   params.fee
+  // );
 
-  return new ethers.Contract(
-    currentPoolAddress,
-    IUniswapV3PoolABI.abi,
-    params.provider
-  );
+  // return new ethers.Contract(
+  //   currentPoolAddress,
+  //   IUniswapV3PoolABI.abi,
+  //   params.provider
+  // );
+  return undefined;
 }
