a71c43736fb3fe2c617e1b1ac6838d3989768167
lnist <16150076+lnist@users.noreply.github.com>
2023-04-27 05:54:20 +0200

    fix(esbuild): fix resolving commonlib from multiple paths

--- packages/mangrove.js/esbuild.mjs
+++ packages/mangrove.js/esbuild.mjs
@@ -2,7 +2,35 @@
 const BrowserBuildPath = "./dist/browser/mangrove.min.js";
 
 import { build } from "esbuild";
-import resolve from "esbuild-plugin-resolve";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const currentFilePath = fileURLToPath(import.meta.url);
+
+function getPath(absoluteResolvePath, shim) {
+  const relativePath = path.relative(
+    absoluteResolvePath,
+    path.dirname(currentFilePath)
+  );
+  return path.join(absoluteResolvePath, relativePath, shim);
+}
+
+const shimOnResolvePlugin = {
+  name: "shimOnResolvePlugin",
+  setup(build) {
+    build.onResolve({ filter: /^@mangrovedao\/commonlib.js$/ }, (args) => {
+      return { path: getPath(args.resolveDir, "shims/commonlib.ts") };
+    });
+
+    build.onResolve({ filter: /^\.\/util\/readJsonWallet$/ }, (args) => {
+      return { path: getPath(args.resolveDir, "shims/readJsonWallet.ts") };
+    });
+
+    build.onResolve({ filter: /^\.\/util\/test\/TestMaker$/ }, (args) => {
+      return { path: getPath(args.resolveDir, "shims/TestMaker.ts") };
+    });
+  },
+};
 
 build({
   entryPoints: ["./src/index.ts"],
@@ -15,11 +43,5 @@ build({
   footer: {
     js: "module.exports = Mangrove;",
   },
-  plugins: [
-    resolve({
-      "@mangrovedao/commonlib.js": "../../shims/commonlib.ts",
-      "./util/readJsonWallet": "../shims/readJsonWallet.ts",
-      "./util/test/TestMaker": "../shims/TestMaker.ts",
-    }),
-  ],
+  plugins: [shimOnResolvePlugin],
 });
