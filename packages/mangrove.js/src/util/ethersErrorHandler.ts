3432a7ba3a5bce691135db90c42e7b57333c04f0
Louis-Amas <amaslouis@protonmail.com>
2023-05-10 14:31:16 +0200

    fix: improve logging

--- packages/mangrove.js/src/util/ethersErrorHandler.ts
+++ packages/mangrove.js/src/util/ethersErrorHandler.ts
@@ -1,14 +1,12 @@
 import { EventEmitter } from "stream";
+import logger from "./logger";
 
 export const RELIABLE_PROVIDER_ERROR_CHANNEL = `reliableProviderError`;
 
 export const onEthersError =
   (emitter: EventEmitter) =>
   (error: any): boolean => {
-    if (error instanceof Error) {
-      // do nothing for nous
-    }
-    console.error(error);
+    logger.error(`error: ${error}`);
     emitter.emit(RELIABLE_PROVIDER_ERROR_CHANNEL, error);
     return false;
   };
