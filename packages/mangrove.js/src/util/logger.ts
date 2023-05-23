fe2548a3735df3cd6b44fb89d991741914cf55ec
Louis-Amas <amaslouis@protonmail.com>
2023-05-23 19:07:24 +0200

    fix: enable normal logging for RES

--- packages/mangrove.js/src/util/logger.ts
+++ packages/mangrove.js/src/util/logger.ts
@@ -3,35 +3,11 @@ import { enableLogging as reliableEventSubscriberEnableLogging } from "@mangrove
 
 let loggingEnabled = false;
 
-const defaultLogger = {
-  info: (...objs: any): void => {
-    console.error("[info]", ...objs);
-  },
-
-  debug: (...objs: any): void => {
-    console.error("[debug]", ...objs);
-  },
-
-  trace: (...objs: any): void => {
-    console.error("[trace]", ...objs);
-  },
-
-  warn: (...objs: any): void => {
-    console.error("[warn]", ...objs);
-  },
-
-  error: (...objs: any): void => {
-    console.error("[errror]", ...objs);
-  },
-};
-
 const logLevel = process.env["LOG_LEVEL"] ? process.env["LOG_LEVEL"] : "debug";
 export const logger = createConsoleLogger(() => loggingEnabled, logLevel);
 
 export function enableLogging(): void {
-  console.error("enableLogging");
-  console.log("enableLogging");
-  reliableEventSubscriberEnableLogging(defaultLogger);
+  reliableEventSubscriberEnableLogging(logger);
   loggingEnabled = true;
 }
 export function disableLogging(): void {
