2938161d8c4856f9b5892a03a0f1dd2762803567
Louis-Amas <amaslouis@protonmail.com>
2023-05-23 15:00:20 +0200

    fix: enable logging for the UI by hardcoding loglevel

--- packages/mangrove.js/src/util/logger.ts
+++ packages/mangrove.js/src/util/logger.ts
@@ -3,11 +3,33 @@ import { enableLogging as reliableEventSubscriberEnableLogging } from "@mangrove
 
 let loggingEnabled = false;
 
-const logLevel = process.env["LOG_LEVEL"] ? process.env["LOG_LEVEL"] : "debug";
+const defaultLogger = {
+  info: (...objs: any): void => {
+    console.error("[info]", ...objs);
+  },
+
+  debug: (...objs: any): void => {
+    console.error("[debug]", ...objs);
+  },
+
+  trace: (...objs: any): void => {
+    console.error("[trace]", ...objs);
+  },
+
+  warn: (...objs: any): void => {
+    console.error("[warn]", ...objs);
+  },
+
+  error: (...objs: any): void => {
+    console.error("[errror]", ...objs);
+  },
+};
+
+const logLevel = "debug"; // process.env["LOG_LEVEL"] ? process.env["LOG_LEVEL"] : "debug";
 export const logger = createConsoleLogger(() => loggingEnabled, logLevel);
 
 export function enableLogging(): void {
-  reliableEventSubscriberEnableLogging(logger);
+  reliableEventSubscriberEnableLogging(defaultLogger);
   loggingEnabled = true;
 }
 export function disableLogging(): void {
