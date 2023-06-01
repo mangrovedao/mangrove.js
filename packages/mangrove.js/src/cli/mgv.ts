06aed4f92706d10845a4b48aa9c74df8c2580996
Adrien Husson <adhusson@gmail.com>
2023-06-01 15:17:10 +0200

    fix: restore deal cmd, prevent future mistyping, add required options

--- packages/mangrove.js/src/cli/mgv.ts
+++ packages/mangrove.js/src/cli/mgv.ts
@@ -9,12 +9,14 @@ import * as dealCmd from "./commands/dealCmd";
 
 const ENV_VAR_PREFIX = "MGV";
 
+type StrictCM = yargs.CommandModule & { builder: (...args: any[]) => any };
+
 yargs
-  .command(parrotCmd)
-  .command(printCmd)
-  .command(retractCmd)
-  .command(dealCmd) // note: node subcommand env vars are prefixed with MGV_NODE instead of MGV_
-  .command(nodeCmd) // note: node subcommand env vars are prefixed with MGV_NODE instead of MGV_
+  .command(parrotCmd as StrictCM)
+  .command(printCmd as StrictCM)
+  .command(retractCmd as StrictCM)
+  .command(dealCmd as StrictCM) // note: node subcommand env vars are prefixed with MGV_NODE instead of MGV_
+  .command(nodeCmd as StrictCM) // note: node subcommand env vars are prefixed with MGV_NODE instead of MGV_
   .strictCommands()
   .demandCommand(1, "You need at least one command before moving on")
   .env(ENV_VAR_PREFIX) // Environment variables prefixed with 'MGV_' are parsed as arguments, see .env([prefix])
