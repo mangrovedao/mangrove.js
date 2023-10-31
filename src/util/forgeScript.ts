19301f3e12b047524440ad45e0686ef3d9e1e900
Louis-Amas <amaslouis@protonmail.com>
2023-10-31 10:45:13 +0100

    fix(tests): remove FOUNDRY_PROFILE=no_env_vars (#1568)

--- src/util/forgeScript.ts
+++ src/util/forgeScript.ts
@@ -10,12 +10,6 @@ export function execForgeCmd(
   pipe?: any,
   handler?: any
 ) {
-  // Foundry needs these RPC urls specified in foundry.toml to be available, else it complains
-  env = {
-    ...env,
-    FOUNDRY_PROFILE: "no_env_vars",
-  };
-
   if (typeof pipe === "undefined") {
     pipe = true;
   }
