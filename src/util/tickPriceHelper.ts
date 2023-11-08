e7da039ddc2a61b2e27ddbc276405918304d7346
lnist <16150076+lnist@users.noreply.github.com>
2023-11-08 16:51:32 +0100

    fix almost all

--- src/util/tickPriceHelper.ts
+++ src/util/tickPriceHelper.ts
@@ -1,8 +1,10 @@
-import { BigNumber } from "ethers";
+import { BigNumber, BigNumberish } from "ethers";
 import { TickLib } from "./coreCalculations/TickLib";
 import Market from "../market";
 
 import Big from "big.js";
+import { Bigish } from "../types";
+import UnitCalculations from "./unitCalculations";
 
 class TickPriceHelper {
   ba: Market.BA;
@@ -11,6 +13,11 @@ class TickPriceHelper {
     quote: { decimals: number };
   };
 
+  /**
+   * Ctor
+   * @param ba bids or asks
+   * @param market the decimals for the market
+   */
   constructor(
     ba: Market.BA,
     market: { base: { decimals: number }; quote: { decimals: number } }
@@ -20,44 +27,98 @@ class TickPriceHelper {
   }
 
   /**
-   * Calculate the price at a given tick.
-   * @param ba bids or asks
+   * Calculates the price at a given tick.
    * @param tick tick to calculate price for
    * @returns price at tick
    */
-  priceFromTick(tick: BigNumber): Big {
-    const priceFromTick = TickLib.priceFromTick(tick);
+  priceFromTick(tick: BigNumberish): Big {
+    const priceFromTick = TickLib.priceFromTick(
+      BigNumber.from(this.ba === "bids" ? -tick : tick)
+    );
     const p = Big(10).pow(
       Math.abs(this.market.base.decimals - this.market.quote.decimals)
     );
 
     let priceWithCorrectDecimals: Big;
     if (this.ba === "bids") {
-      priceWithCorrectDecimals = priceFromTick.div(p);
-    } else {
       priceWithCorrectDecimals = priceFromTick.mul(p);
+    } else {
+      priceWithCorrectDecimals = priceFromTick.div(p);
     }
     return priceWithCorrectDecimals;
   }
 
   /**
-   * Calculate the tick at a given price.
-   * @param ba bids or asks
+   * Calculates the tick at a given price.
    * @param price price to calculate tick for
    * @returns tick for price
    */
-  tickFromPrice(price: Big): BigNumber {
+  tickFromPrice(price: Bigish): BigNumber {
     const p = Big(10).pow(
       Math.abs(this.market.base.decimals - this.market.quote.decimals)
     );
 
     let priceAdjustedForDecimals: Big;
     if (this.ba === "bids") {
-      priceAdjustedForDecimals = price.mul(p);
+      priceAdjustedForDecimals = Big(price).mul(p);
     } else {
-      priceAdjustedForDecimals = price.div(p);
+      priceAdjustedForDecimals = Big(price).div(p);
     }
-    return TickLib.getTickFromPrice(priceAdjustedForDecimals);
+    const askTick = TickLib.getTickFromPrice(priceAdjustedForDecimals);
+    if (this.ba === "bids") {
+      return askTick.mul(-1);
+    }
+    return askTick;
+  }
+
+  /**
+   * Calculates the inbound amount from an outbound amount at a given tick.
+   * @param tick tick to calculate the amount for
+   * @param outboundAmount amount to calculate the inbound amount for
+   * @param roundUp whether to round up (true) or down (falsy)
+   * @returns inbound amount.
+   */
+  inboundFromOutbound(
+    tick: BigNumberish,
+    outboundAmount: Bigish,
+    roundUp?: boolean
+  ) {
+    const rawOutbound = UnitCalculations.toUnits(
+      outboundAmount,
+      this.ba == "asks" ? this.market.base.decimals : this.market.quote.decimals
+    );
+    const rawInbound = (
+      roundUp ? TickLib.inboundFromOutboundUp : TickLib.inboundFromOutbound
+    )(BigNumber.from(tick), rawOutbound);
+    return UnitCalculations.fromUnits(
+      rawInbound,
+      this.ba == "asks" ? this.market.quote.decimals : this.market.base.decimals
+    );
+  }
+
+  /**
+   * Calculates the outbound amount from an inbound amount at a given tick.
+   * @param tick tick to calculate the amount for
+   * @param inboundAmount amount to calculate the outbound amount for
+   * @param roundUp whether to round up (true) or down (falsy)
+   * @returns inbound amount.
+   */
+  outboundFromInbound(
+    tick: BigNumberish,
+    inboundAmount: Bigish,
+    roundUp?: boolean
+  ) {
+    const rawInbound = UnitCalculations.toUnits(
+      inboundAmount,
+      this.ba == "asks" ? this.market.quote.decimals : this.market.base.decimals
+    );
+    const rawOutbound = (
+      roundUp ? TickLib.outboundFromInboundUp : TickLib.outboundFromInbound
+    )(BigNumber.from(tick), rawInbound);
+    return UnitCalculations.fromUnits(
+      rawOutbound,
+      this.ba == "asks" ? this.market.base.decimals : this.market.quote.decimals
+    );
   }
 }
 
--- test/unit/tickPriceHelper.unit.test.ts
+++ test/unit/tickPriceHelper.unit.test.ts
@@ -1,19 +1,20 @@
 import assert from "assert";
 import { Big } from "big.js";
-import { expect } from "chai";
 import { describe, it } from "mocha";
 import { Market } from "../../src";
 import { BigNumber } from "ethers";
 import TickPriceHelper from "../../src/util/tickPriceHelper";
+import { Bigish } from "../../src/types";
+import { bidsAsks } from "../../src/util/test/mgvIntegrationTestUtil";
 
-describe("TickPriceHelper unit tests suite", () => {
+describe(`${TickPriceHelper.prototype.constructor.name} unit tests suite`, () => {
   const priceAndTickPairs: {
     args: {
       ba: Market.BA;
       market: { base: { decimals: number }; quote: { decimals: number } };
     };
     tick: number;
-    price: Big;
+    price: Bigish;
   }[] = [
     {
       args: {
@@ -35,7 +36,7 @@ describe("TickPriceHelper unit tests suite", () => {
         },
       },
       tick: 0,
-      price: Big(1),
+      price: 1,
     },
     {
       args: {
@@ -46,7 +47,7 @@ describe("TickPriceHelper unit tests suite", () => {
         },
       },
       tick: 0,
-      price: Big("1e-12"),
+      price: "1e-12",
     },
     {
       args: {
@@ -83,7 +84,7 @@ describe("TickPriceHelper unit tests suite", () => {
     },
     {
       args: {
-        ba: "bids",
+        ba: "asks",
         market: {
           base: { decimals: 18 },
           quote: { decimals: 18 },
@@ -101,7 +102,7 @@ describe("TickPriceHelper unit tests suite", () => {
         },
       },
       tick: -75170,
-      price: Big("0.000543966"),
+      price: Big("1838.350856476"),
     },
     {
       args: {
@@ -111,7 +112,7 @@ describe("TickPriceHelper unit tests suite", () => {
           quote: { decimals: 18 },
         },
       },
-      tick: 414487,
+      tick: -414487,
       price: Big("1.000096034"),
     },
     {
@@ -122,49 +123,47 @@ describe("TickPriceHelper unit tests suite", () => {
           quote: { decimals: 0 },
         },
       },
-      tick: -414487,
+      tick: 414487,
       price: Big("1e-36"),
     },
   ];
 
-  const comparisonPrecision = 9;
+  const comparisonPrecision = 8;
 
-  describe("priceFromTick", () => {
+  describe(TickPriceHelper.prototype.priceFromTick.name, () => {
     priceAndTickPairs.forEach(({ args, tick, price }) => {
-      it(`returns price=${price} for tick ${tick} with base decimals: ${args.market.base.decimals}, quote decimals: ${args.market.quote.decimals} (${args.ba} semibook)`, async function () {
+      it(`returns price=${price} for tick ${tick} with base decimals: ${args.market.base.decimals}, quote decimals: ${args.market.quote.decimals} (${args.ba} semibook)`, () => {
         // Arrange
         const tickPriceHelper = new TickPriceHelper(args.ba, args.market);
 
         // Act
         const result = tickPriceHelper.priceFromTick(BigNumber.from(tick));
         // Assert
-        assert.ok(
-          result
-            .round(comparisonPrecision)
-            .eq(price.round(comparisonPrecision)),
-          `expected ${price} but got ${result}`
+        assert.equal(
+          result.round(comparisonPrecision).toString(),
+          Big(price).round(comparisonPrecision).toString()
         );
       });
     });
   });
 
-  describe("tickFromPrice", () => {
+  describe(TickPriceHelper.prototype.tickFromPrice.name, () => {
     priceAndTickPairs.forEach(({ args, tick, price }) => {
-      it(`returns tick=${tick} for price ${price} with base decimals: ${args.market.base.decimals}, quote decimals: ${args.market.quote.decimals} (${args.ba} semibook)) `, async function () {
+      it(`returns tick=${tick} for price ${price} with base decimals: ${args.market.base.decimals}, quote decimals: ${args.market.quote.decimals} (${args.ba} semibook)) `, () => {
         // Arrange
         const tickPriceHelper = new TickPriceHelper(args.ba, args.market);
 
         // Act
         const result = tickPriceHelper.tickFromPrice(price);
         // Assert
-        assert.ok(result.eq(tick), `expected ${tick} but got ${result}`);
+        assert.equal(tick, result.toNumber());
       });
     });
   });
 
   describe("tickFromPrice is inverse of priceFromTick (up to tick-step)", () => {
     priceAndTickPairs.forEach(({ args, tick }) => {
-      it(`returns tick=${tick} for priceFromTick(..., priceFromTick(..., ${tick}))) with base decimals: ${args.market.base.decimals}, quote decimals: ${args.market.quote.decimals} for ${args.ba} semibook`, async function () {
+      it(`returns tick=${tick} for priceFromTick(..., priceFromTick(..., ${tick}))) with base decimals: ${args.market.base.decimals}, quote decimals: ${args.market.quote.decimals} for ${args.ba} semibook`, () => {
         // Arrange
         const tickPriceHelper = new TickPriceHelper(args.ba, args.market);
 
@@ -184,7 +183,7 @@ describe("TickPriceHelper unit tests suite", () => {
 
   describe("priceFromTick is inverse of tickFromPrice (up to tick-step)", () => {
     priceAndTickPairs.forEach(({ args, price }) => {
-      it(`returns price=${price} for tickFromPrice(..., tickFromPrice(..., ${price}))) with base decimals: ${args.market.base.decimals}, quote decimals: ${args.market.quote.decimals} for ${args.ba} semibook`, async function () {
+      it(`returns price=${price} for tickFromPrice(..., tickFromPrice(..., ${price}))) with base decimals: ${args.market.base.decimals}, quote decimals: ${args.market.quote.decimals} for ${args.ba} semibook`, () => {
         // Arrange
         const tickPriceHelper = new TickPriceHelper(args.ba, args.market);
 
@@ -194,14 +193,14 @@ describe("TickPriceHelper unit tests suite", () => {
         );
 
         const resultPriceTickPlusOne = tickPriceHelper.priceFromTick(
-          tickPriceHelper.tickFromPrice(price).add(1)
+          tickPriceHelper.tickFromPrice(price).add(args.ba == "bids" ? -1 : 1)
         );
 
         const resultPriceTickMinusOne = tickPriceHelper.priceFromTick(
-          tickPriceHelper.tickFromPrice(price).sub(1)
+          tickPriceHelper.tickFromPrice(price).add(args.ba == "bids" ? 1 : -1)
         );
 
-        const roundedPrice = price.round(comparisonPrecision);
+        const roundedPrice = Big(price).round(comparisonPrecision);
 
         // Assert
         assert.ok(
@@ -211,6 +210,62 @@ describe("TickPriceHelper unit tests suite", () => {
             resultPriceTickPlusOne.round(comparisonPrecision).gte(roundedPrice),
           `expected ${price} to be within one tick-step of ${resultPrice}`
         );
+      });
+    });
+  });
+
+  xdescribe(TickPriceHelper.prototype.inboundFromOutbound.name, () => {
+    it("sdkprice", () => {
+      const bidTickPriceHelper = new TickPriceHelper("bids", {
+        base: { decimals: 6 },
+        quote: { decimals: 6 },
+      });
+      const askTickPriceHelper = new TickPriceHelper("asks", {
+        base: { decimals: 6 },
+        quote: { decimals: 6 },
+      });
+      const b = bidTickPriceHelper.tickFromPrice(2);
+      const a = askTickPriceHelper.tickFromPrice(2);
+
+      console.log(a.toString(), b.toString());
+    });
+
+    it("handles simple case of 1,1,1", () => {
+      const tickPriceHelper = new TickPriceHelper("bids", {
+        base: { decimals: 6 },
+        quote: { decimals: 6 },
+      });
+
+      const inbound = tickPriceHelper
+        .inboundFromOutbound(tickPriceHelper.tickFromPrice(2), 10)
+        .toNumber();
+
+      assert.equal(inbound, 20);
+    });
+    bidsAsks.forEach((ba) => {
+      // base, quote, price
+      [
+        [1, 2, 2],
+        [2, 1, 0.5],
+        [3, 3, 0.5],
+      ].forEach(([base, quote, price]) => {
+        it(`ba=${ba} base=${base} quote=${quote} price=${price}`, () => {
+          // Arrange
+          const tickPriceHelper = new TickPriceHelper(ba, {
+            base: { decimals: 1 },
+            quote: { decimals: 2 },
+          });
+
+          // Act
+          const [outbound, expectedInbound] =
+            ba == "asks" ? [base, quote] : [quote, base];
+          const result = tickPriceHelper.inboundFromOutbound(
+            tickPriceHelper.tickFromPrice(price),
+            outbound
+          );
+          // Assert
+          assert.equal(result.toNumber(), expectedInbound);
+        });
       });
     });
   });
