491fcd3fabe8635c70ffe0e84c7751416746db9d
Maxence Raballand <dev@maxencerb.com>
2024-03-08 15:22:39 +0100

    Fix gas requirementsé

--- test/unit/configuration.unit.test.ts
+++ test/unit/configuration.unit.test.ts
@@ -78,7 +78,7 @@ describe("Configuration unit tests suite", () => {
     );
     assert.equal(
       configuration.mangroveOrder.getRestingOrderGasreq("local", "aave"),
-      700000,
+      1000000,
     );
     assert.equal(
       configuration.mangroveOrder.getRestingOrderGasreq("maticmum"),
@@ -86,7 +86,7 @@ describe("Configuration unit tests suite", () => {
     );
     assert.equal(
       configuration.mangroveOrder.getRestingOrderGasreq("maticmum", "aave"),
-      700000,
+      1000000,
     );
     assert.equal(
       configuration.mangroveOrder.getRestingOrderGaspriceFactor("local"),
